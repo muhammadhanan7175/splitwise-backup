9003effa83cab17043f09e8bd2262e8b31b558b4
muhammadhanan7175 <muhammadhanan7175@gmail.com>
2023-08-23 03:15:11 -0700

    Added validation for proper email input in login  section

--- src/Pages/Login.js
+++ src/Pages/Login.js
@@ -4,41 +4,69 @@ import { auth } from "../components/Firebase-config";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import './Login.css';
-function Login(){
+
+function Login() {
     const [emailList, setEmailList] = useState([]);
     const direction = useNavigate();
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
 
+    const isEmailValid = (email) => {
+        const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+        return emailPattern.test(email);
+    };
+
     const logout = async () => {
-        await signOut(auth)
-    }
+        await signOut(auth);
+    };
+
     const login = async () => {
+        if (!isEmailValid(loginEmail)) {
+            alert("Improper email entered");
+            return;
+        }
+
         try {
-            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword).then((userCred)=>{
+            const user = await signInWithEmailAndPassword(auth, loginEmail, loginPassword).then((userCred) => {
                 console.log(userCred);
-                emailList.push(loginEmail)
-                
+                setEmailList([...emailList, loginEmail]);
             });
-            
+
         } catch (error) {
             console.log(error.message);
         }
-        console.log( emailList);
-        direction("/Userportal")
-    }
-    return(
-             <>
-                <div className="formContainer">
-                    <form className="form">
-                        <input type="email" placeholder="E-mail Address" id="email" name="email"
-                            value={loginEmail} onChange={(e) =>setLoginEmail(e.target.value)} />
-                        <input type="password" placeholder="Password" id="password" name="password"
-                            value={loginPassword} onChange={(event) => setLoginPassword(event.target.value)} />
-                        <button type="button" onClick={login}>Login</button>
-                    </form>
-                </div>
-            </>
-    )
+        
+        console.log(emailList);
+        direction("/Userportal");
+    };
+
+    return (
+        <>
+            <div className="formContainer">
+                <form className="form">
+                    <input
+                        type="email"
+                        placeholder="E-mail Address"
+                        id="email"
+                        name="email"
+                        value={loginEmail}
+                        onChange={(e) => setLoginEmail(e.target.value)}
+                    />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        id="password"
+                        name="password"
+                        value={loginPassword}
+                        onChange={(event) => setLoginPassword(event.target.value)}
+                    />
+                    <button type="button" onClick={login}>
+                        Login
+                    </button>
+                </form>
+            </div>
+        </>
+    );
 }
-export default Login
+
+export default Login;
